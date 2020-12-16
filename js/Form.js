1585503d5465bcbc19fe7efd13ab7808b68942af
Tejal-Page <71485773+Tejal-Page@users.noreply.github.com>
2020-12-16 11:23:55 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -11,7 +11,7 @@ class Form {
     this.button = createButton('Play');// creates button
     this.greeting = createElement('h2');// header style- element is used when u want to display a text
   }
-  //hides everything, especially when all players have logged in , u want all to disappear
+  //hides everything, especially when all players have logged in , u want all to disappear...i.e in play state
   hide(){
     this.greeting.hide();
     this.button.hide();
@@ -28,7 +28,7 @@ class Form {
     this.input.position(130, 160);
     this.button.position(250, 200);
     
-    //3. press the ouse button
+    //3. press the mouse button
     /* 
     -now here you have tonnes of things to do , which cant mention all tasks to be done in one single brackets.
     - thus turn it into a function
@@ -46,8 +46,8 @@ class Form {
       this.button.hide();
       player.name = this.input.value();  //reads/ extracts value from html element
       playerCount+=1;
-      player.index = playerCount;
-      player.update();
+      player.index = playerCount;//equating index to count of player...this is where the index gets updated...
+      player.update(); //without an argument
       player.updateCount(playerCount);
       this.greeting.html("Hello " + player.name)
       this.greeting.position(130, 100);
@@ -60,5 +60,7 @@ class Form {
 - USED AS CALL BACK FUCNTION
 - CAN ACCESS THE ENTIRE CLASSS, EVEN THE CONTRUCTOR
 - this.button and this.inptu can only be accessed by this function not by arrow
+- the this. keyword in a regular function , refers to object which is calling the function, so eg, button is calling mousepressed, so it referes to button...
+- we dont want that, so in arraow , the this keyword referes to original object, i.e form object not button.
 */
 
