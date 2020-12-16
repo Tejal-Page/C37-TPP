abcc593b6cb0a5c1033a4a5f32ca68b88305dc63
Tejal-Page <71485773+Tejal-Page@users.noreply.github.com>
2020-12-16 11:37:00 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -50,22 +50,31 @@ class Game {
 */
   
   play(){
+    //hide the form
     form.hide();
+    //displaying text to start the game
     textSize(30);
     text("Game Start", 120, 100)
-    Player.getPlayerInfo();
-
+    Player.getPlayerInfo(); //class name(Player), because it is a static function- called by class name not object name
+    
+  //undefined- means to check if all players have logged in or not
     if(allPlayers !== undefined){
-      var display_position = 130;
+      
+      var display_position = 130; //y position of text where u want to display all players info
+      
+      //we want to display allplayers info on the screen one below the other. thus text y position will keep changing
+      //for loop is used to change the y position
       for(var plr in allPlayers){
+        //color change- active player is red, others black
         if (plr === "player" + player.index)
           fill("red")
         else
           fill("black");
 
-        display_position+=20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        display_position+=20; // this is where the y position increases by 20
+        textSize(15); //size change
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position) //displaying text, allPlayers[plr].name....allPLayers is an array so [plr]
+        //refers to which player
       }
     }
 
